75acefdf782108e3f1fcf2515582a6417afd126e
Ashish Bhungaliya <ashish@softwebsolutions.com>
2016-09-28 17:14:06 +0530

    Init

---
+++ GoogleSearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+
+namespace TestGoogleSearch
+{
+    public class GoogleSearch
+    {
+        //API Key
+        private static string API_KEY = "AIzaSyCyqAm432caVHD6ycUEWTbCNtg4rD_ao8Y";
+
+        //The custom search engine identifier
+        private static string cx = "015598178761323117960:sbbkk2__0lo";
+
+        public static CustomsearchService Service = new CustomsearchService(
+            new BaseClientService.Initializer
+        {
+            ApplicationName = "ISBNBookCsutomSearch",
+            ApiKey = API_KEY,
+        });
+
+        public static IList<Result> Search(string searchText, string fileType = "", int topResult = 10)
+        {
+            Console.WriteLine("Executing google custom search for: {0} ...", searchText);
+
+            Search search;
+            List<Result> lstResult = new List<Result>();
+            CseResource.ListRequest listRequest = Service.Cse.List(searchText);
+            listRequest.Cx = cx;
+            listRequest.Num = 10;
+            if (!string.IsNullOrEmpty(fileType))
+            {
+                listRequest.FileType = fileType;
+            }
+
+            int net = topResult / 10;
+            int odd = topResult % 10;
+            for (int i = 1; i <= net; i++)
+            {
+                search = listRequest.Execute();
+                lstResult.AddRange(search.Items);
+                listRequest.Start = i*10;
+            }
+
+            if (odd > 0)
+            {
+                listRequest.Num = odd;
+                search = listRequest.Execute();
+                lstResult.AddRange(search.Items);
+            }
+
+            return lstResult;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Customsearch.v1.Data;
+using System.Net;
+
+namespace TestGoogleSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Seach Text for PDF: ");
+            string searchText = Convert.ToString(Console.ReadLine());
+
+            Console.Write("\nHow many result(s): ");
+            int top = Convert.ToInt32(Console.ReadLine());
+
+           
+            var results = GoogleSearch.Search(searchText, "pdf", top);
+            Console.WriteLine(string.Format("\n---------------- Top {0} Results ------------------\n", top));
+            for (int i = 0; i < results.Count; i++)
+            {
+                Console.WriteLine("({1})   Title: {0}", results[i].Title,i+1);
+                Console.WriteLine("      Link: {0} \n", results[i].Link);
+                //if (result.Mime == "application/pdf")
+                //{
+                //    SaveFile(result.FormattedUrl, result.FormattedUrl.Split('/').LastOrDefault().Replace("%20", "-"));
+                //}
+            }
+            Console.WriteLine("----------------- END ------------------");
+            Console.ReadKey();
+        }
+
+        private static bool SaveFile(string url, string fileName)
+        {
+            try
+            {
+                string targetFolder = @"C:\Users\jayp\Desktop\admindll\";
+                WebClient Client = new WebClient();
+                Client.DownloadFile(url, targetFolder + fileName);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+}
